716ef08ab2bbecf081f0569c29cb530627f31773
ahndongjoo <dksehdwn11@naver.com>
2022-05-25 18:23:31 +0900

    -update-nav git commit -m -update-nav git commit -m -update-nav
    git commit -m -update-nav

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router.js'
+import { store } from './store.js'
 
-createApp(App).mount('#app')
+// import '/@static/css/reset.css'
+
+createApp(App).use(router , store).mount('#app');
